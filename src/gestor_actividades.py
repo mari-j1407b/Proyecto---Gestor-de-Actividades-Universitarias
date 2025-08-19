70519cb4c7cfd1756955fc91f0a7d6b85e7ed108
onemor3user <lunacaldes@gmail.com>
2025-08-18 20:54:33 -0600

    listar actividades

--- src/gestor_actividades.py
+++ src/gestor_actividades.py
@@ -12,3 +12,12 @@ class actividades:
         return f"Nombre de la actividad {self.actividad} dia {self.dia} Mes {self.mes} año {self.año} con prioridad {self.prioridad}"
 
 actividades_universitarias = []
+
+    elif opc == 3:
+        os.system('cls')
+        print("Se eligio la opcio 3, buscra actividades por mes, año, dia, o categotia")
+        mes_año = input("Ingrese el mes o año a buscar: ").lower()
+        for actividad in actividades_universitarias:
+            if mes_año in actividad.actividad or actividad.dia == mes_año or actividad.mes == mes_año or actividad.año == mes_año or actividad.prioridad == mes_año:
+                print(actividad)
+        passgt
