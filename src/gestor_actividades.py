f3d205d32ab6c226e4e53b82162ef9aae255a52b
Cracy456 <mariomatheu9@gmail.com>
2025-08-18 18:34:31 -0600

    Agregando los atributos y el constructor

--- src/gestor_actividades.py
+++ src/gestor_actividades.py
@@ -2,3 +2,9 @@ import os
 
 class actividades:
     def __init_(self, actividad, dia, mes, año, prioridad):
+        selfactividad = actividad
+        self.dia = dia
+        self.mes = mes
+        self.año = año
+        self.prioridad = prioridad
+        
